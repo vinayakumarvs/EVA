dfae3307138f6f4f36fdb319a43f1bcfcd999ae6
vinayakumarvs <vinayakumarvs@users.noreply.github.com>
2020-02-03 21:58:15 +0530

    Delete

--- Assignment 15/repo/Data/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-from .data import *
-from .aug import *
-
--- Assignment 15/repo/Model/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-from ..model_arch import DavidNet
-
